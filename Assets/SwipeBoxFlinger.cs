7eb3492f71170a3930c6c9381b77aef08ca05e20
Jonathan Levin <jonmlevin@outlook.com>
2021-01-28 23:50:04 +0200

    throw stuff

--- Assets/SwipeBoxFlinger.cs
+++ Assets/SwipeBoxFlinger.cs
@@ -1,38 +1,81 @@
+using System.Collections;
 using Lean.Touch;
 using UnityEngine;
+using Utils;
+using Utils.ScriptableObjects.Audio;
 
 public class SwipeBoxFlinger : MonoBehaviour
 {
+    [SerializeField] private PackageSpawner[] _spawners;
+    [SerializeField] private RangedFloat[] _screenSplitXs;
     [SerializeField] private float _flingBasePower;
     [Range(0f, 1f)]
     [SerializeField] private float _flingArc;
-    [SerializeField] private PackageBox _boxPrefab;
-    [SerializeField] private Transform _packageSpawner;
-    [SerializeField] private GameObject test;
-    
+
+    // private PackageSpawner[] _spawnersInScene;
+
+    private void Awake()
+    {
+        // _spawnersInScene = FindObjectsOfType<PackageSpawner>();
+    }
+
     // Called from scene
     public void OnLeanFinger(LeanFinger finger)
     {
         Debug.Log($"Swipe: scaledDelta: {finger.SwipeScreenDelta} Age: {finger.Age}");
-        var swipeScreenDelta = finger.SwipeScreenDelta;
-        var normalizedDelta = new Vector2(
-            swipeScreenDelta.x / Screen.width,
-            swipeScreenDelta.y / Screen.height);
+        OnSwipe(finger.StartScreenPosition, finger.ScreenPosition, finger.Age);
+    }
+
+    private void OnSwipe(Vector2 screenStart, Vector2 screenEnd, float time)
+    {
+        var viewportStart = Camera.main.ScreenToViewportPoint(screenStart);
+        var viewportEnd = Camera.main.ScreenToViewportPoint(screenEnd);
+        var normalizedDelta = viewportEnd - viewportStart;
         
-        OnSwipe(normalizedDelta, finger.Age);
-    }
-
-    private void Fling(Vector3 force)
-    {
-        test.transform.rotation = Quaternion.FromToRotation(Vector3.forward, force);
-        var box = Instantiate(_boxPrefab, _packageSpawner, false);
-        box.Fling(force);
-    }
-
-    private void OnSwipe(Vector2 normalizedDelta, float time)
-    {
         Vector3 flingForce = GetFlingForce(normalizedDelta, time);
-        Fling(flingForce);
+        var spawner = FindClosestSpawner(viewportStart);
+        spawner.TryFling(flingForce);
+    }
+
+    private PackageSpawner FindClosestSpawner(Vector3 viewportPos)
+    {
+        Debug.Log($"Find close ({viewportPos}");
+        // var sampleDist = _spawnersInScene[0].transform.position.z;
+        // viewportPos.z = -sampleDist;
+
+        var i = FindIndexContaining(_screenSplitXs, viewportPos.x);
+        return _spawners[i];
+        //
+        // var worldPoint = Camera.main.ViewportToWorldPoint(viewportPos);
+        // DebugStuff.Instance.DrawPoint(worldPoint, 2f);
+        // float SpawnerSqrDist(PackageSpawner spawner)
+        // {
+        //     var spawnerPos = spawner.transform.position;
+        //     var ray = worldPoint - spawnerPos;
+        //     
+        //     Debug.DrawRay(worldPoint, 15f * ray, Color.green, 1.5f);
+        //     return ray.sqrMagnitude;
+        // }
+        //
+        // PackageSpawner minSpawn = null;
+        // var minDist = float.PositiveInfinity;
+        // foreach (var packageSpawner in _spawnersInScene)
+        // {
+        //     if (SpawnerSqrDist(packageSpawner) < minDist) minSpawn = packageSpawner;
+        // }
+        //
+        // return minSpawn;
+    }
+
+    private int FindIndexContaining(RangedFloat[] ranges, float val)
+    {
+        for (var i = 0; i < ranges.Length; i++)
+        {
+            var range = ranges[i];
+            if (range.minValue <= val && val <= range.maxValue) return i;
+        }
+
+        return -1;
     }
 
     private Vector3 GetFlingForce(Vector2 viewPortDelta, float time)
@@ -41,3 +84,20 @@ public class SwipeBoxFlinger : MonoBehaviour
         return force * _flingBasePower * 1f / time;
     }
 }
+
+public class DebugStuff : Singleton<DebugStuff>
+{
+    public void DrawPoint(Vector3 point, float dur)
+    {
+        StartCoroutine(ShowFor(point, dur));
+    }
+
+    private IEnumerator ShowFor(Vector3 point, float dur)
+    {
+        var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        go.transform.position = point;
+        yield return new WaitForSeconds(dur);
+        Destroy(go);
+    }
+}
+
